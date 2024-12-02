b8deeab6e325cd8c24b474c2c934b18401d368ed
Maxwell Schaefer <mrs314@case.edu>
2024-12-01 16:41:52 -0800

    Parsed the input file

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 const eql = std.mem.eql;
+const ArrayList = std.ArrayList;
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
@@ -13,7 +14,7 @@ pub fn main() !void {
 
     // Sort the arrays
 
-    // Accummulate the differences
+    // Accumulate the differences
 
     try stdout.print("Final answer: {d}\n", .{out});
 
@@ -28,9 +29,41 @@ test "read file test" {
     const expected = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
     const bytes_read = try file.readAll(&buffer);
 
-    std.debug.print("{s}\n", .{buffer[0..bytes_read]});
-
     try expect(eql(u8, buffer[0..bytes_read], expected));
+}
+
+fn read_data(file_name: []const u8, list_1: *ArrayList(u32), list_2: *ArrayList(u32)) !void {
+    const file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    var buffer: [100]u8 = undefined;
+    const reader = file.reader();
+
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        const delim = [_]u8{' '};
+        var tokens = std.mem.tokenize(u8, line, &delim);
+
+        const l1 = tokens.next().?;
+        const l2 = tokens.next().?;
+
+        // std.debug.print("{s} {s}\n", .{ l1, l2 });
+
+        try list_1.append(try std.fmt.parseUnsigned(u32, l1, 10));
+        try list_2.append(try std.fmt.parseUnsigned(u32, l2, 10));
+
+        try expect(tokens.next() == null);
+    }
+}
+
+test "read data test" {
+    const allocator = std.heap.page_allocator;
+
+    var l1 = ArrayList(u32).init(allocator);
+    var l2 = ArrayList(u32).init(allocator);
+    try read_data("../data/day1_test.txt", &l1, &l2);
+
+    // std.debug.print("List1: {any}\n", .{l1.items});
+    // std.debug.print("List2: {any}\n", .{l2.items});
 }
 
 test "simple test" {
