25c8e22ba3bdb0f286da0d2f1d6b5902cfbf3a60
Maxwell Schaefer <mrs314@case.edu>
2024-12-01 17:31:02 -0800

    Part 2 completed

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,40 @@ const std = @import("std");
 const expect = std.testing.expect;
 const eql = std.mem.eql;
 const ArrayList = std.ArrayList;
+const sort = std.mem.sort;
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const out: u64 = undefined;
-
     // Read the input data into two arrays/lists
+    const allocator = std.heap.page_allocator;
+
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var l2 = ArrayList(u64).init(allocator);
+    defer l2.deinit();
+    try readData("data/day1.txt", &l1, &l2);
 
     // Sort the arrays
+    sortArrayListsDesc(&l1, &l2);
 
     // Accumulate the differences
+    const dist = getDistance(&l1, &l2);
 
-    try stdout.print("Final answer: {d}\n", .{out});
+    // Part 2
+
+    // Create a hash map storing a count of the numbers
+    var map = std.AutoHashMap(u64, u64).init(allocator);
+    defer map.deinit();
+
+    try constructFreqMap(&l2, &map);
+
+    const similarity = getSimilarityScore(&l1, &map);
+
+    try stdout.print("Distance: {any}\n", .{dist});
+    try stdout.print("Similarity: {any}\n", .{similarity});
 
     try bw.flush(); // don't forget to flush!
 }
@@ -32,7 +51,7 @@ test "read file test" {
     try expect(eql(u8, buffer[0..bytes_read], expected));
 }
 
-fn read_data(file_name: []const u8, list_1: *ArrayList(u32), list_2: *ArrayList(u32)) !void {
+fn readData(file_name: []const u8, list_1: *ArrayList(u64), list_2: *ArrayList(u64)) !void {
     const file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
 
@@ -48,8 +67,8 @@ fn read_data(file_name: []const u8, list_1: *ArrayList(u32), list_2: *ArrayList(
 
         // std.debug.print("{s} {s}\n", .{ l1, l2 });
 
-        try list_1.append(try std.fmt.parseUnsigned(u32, l1, 10));
-        try list_2.append(try std.fmt.parseUnsigned(u32, l2, 10));
+        try list_1.append(try std.fmt.parseUnsigned(u64, l1, 10));
+        try list_2.append(try std.fmt.parseUnsigned(u64, l2, 10));
 
         try expect(tokens.next() == null);
     }
@@ -58,12 +77,130 @@ fn read_data(file_name: []const u8, list_1: *ArrayList(u32), list_2: *ArrayList(
 test "read data test" {
     const allocator = std.heap.page_allocator;
 
-    var l1 = ArrayList(u32).init(allocator);
-    var l2 = ArrayList(u32).init(allocator);
-    try read_data("../data/day1_test.txt", &l1, &l2);
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var l2 = ArrayList(u64).init(allocator);
+    defer l2.deinit();
+    try readData("../data/day1_test.txt", &l1, &l2);
 
     // std.debug.print("List1: {any}\n", .{l1.items});
     // std.debug.print("List2: {any}\n", .{l2.items});
+}
+
+fn sortArrayListsDesc(list_1: *ArrayList(u64), list_2: *ArrayList(u64)) void {
+    std.mem.sort(u64, list_1.items, {}, std.sort.asc(u64));
+    std.mem.sort(u64, list_2.items, {}, std.sort.asc(u64));
+}
+
+test "test sorting" {
+    const allocator = std.heap.page_allocator;
+
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var l2 = ArrayList(u64).init(allocator);
+    defer l2.deinit();
+
+    const vals_1 = [_]u64{ 3, 4, 2, 1, 3, 3 };
+    try l1.appendSlice(vals_1[0..]);
+
+    const vals_2 = [_]u64{ 4, 3, 5, 3, 9, 3 };
+    try l2.appendSlice(vals_2[0..]);
+
+    sortArrayListsDesc(&l1, &l2);
+
+    const out_1 = [_]u64{ 1, 2, 3, 3, 3, 4 };
+
+    const out_2 = [_]u64{ 3, 3, 3, 4, 5, 9 };
+
+    try expect(eql(u64, l1.items, out_1[0..]));
+    try expect(eql(u64, l2.items, out_2[0..]));
+}
+
+fn getDistance(list_1: *ArrayList(u64), list_2: *ArrayList(u64)) u64 {
+    var out: u64 = 0;
+    for (list_1.items, list_2.items) |l1, l2| {
+        const diff = if (l1 > l2) (l1 - l2) else (l2 - l1);
+        out += diff;
+    }
+
+    return out;
+}
+
+test "test distance" {
+    const allocator = std.heap.page_allocator;
+
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var l2 = ArrayList(u64).init(allocator);
+    defer l2.deinit();
+
+    const vals_1 = [_]u64{ 1, 2, 3, 3, 3, 4 };
+    try l1.appendSlice(vals_1[0..]);
+
+    const vals_2 = [_]u64{ 3, 3, 3, 4, 5, 9 };
+    try l2.appendSlice(vals_2[0..]);
+
+    const dist = getDistance(&l1, &l2);
+
+    try expect(dist == 11);
+}
+
+fn constructFreqMap(list_1: *ArrayList(u64), map: *std.AutoHashMap(u64, u64)) !void {
+    for (list_1.items) |i| {
+        // Increment the previous value or set it to 1
+        const prev_val = map.get(i) orelse 0;
+        try map.put(i, prev_val + 1);
+    }
+}
+
+test "construct frequency map" {
+    const allocator = std.testing.allocator;
+
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var map = std.AutoHashMap(u64, u64).init(allocator);
+    defer map.deinit();
+
+    const vals_1 = [_]u64{ 3, 4, 2, 1, 3, 3 };
+    try l1.appendSlice(vals_1[0..]);
+
+    try constructFreqMap(&l1, &map);
+
+    try expect(map.count() == 4);
+}
+
+fn getSimilarityScore(list_1: *ArrayList(u64), map: *std.AutoHashMap(u64, u64)) u64 {
+    var out: u64 = 0;
+
+    for (list_1.items) |l1| {
+        out += l1 * (map.get(l1) orelse 0); // Multiply by zero if the entry wasn't in the other list
+    }
+
+    return out;
+}
+
+test "test similarity" {
+    const allocator = std.heap.page_allocator;
+
+    var l1 = ArrayList(u64).init(allocator);
+    defer l1.deinit();
+    var l2 = ArrayList(u64).init(allocator);
+    defer l2.deinit();
+
+    const vals_1 = [_]u64{ 1, 2, 3, 3, 3, 4 };
+    try l1.appendSlice(vals_1[0..]);
+
+    const vals_2 = [_]u64{ 3, 3, 3, 4, 5, 9 };
+    try l2.appendSlice(vals_2[0..]);
+
+    var map = std.AutoHashMap(u64, u64).init(allocator);
+    defer map.deinit();
+
+    try constructFreqMap(&l2, &map);
+
+    const similarity = getSimilarityScore(&l1, &map);
+
+    try expect(similarity == 31);
 }
 
 test "simple test" {
